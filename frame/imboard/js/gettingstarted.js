2ff165eff7474612831e10d65fe0ebc5736caf8d
Alprensia <tonite32@gmail.com>
2015-07-14 18:51:14 +0900

    임보드 홈페이지 시작하기 화면 추가

---
+++ frame/imboard/js/gettingstarted.js
@@ -0,0 +1,42 @@
+$(document).ready(function()
+{
+	$("#contentList").compile({top : 20, direction : "vertical"});
+	$("#contentList li:first").addClass("selected");
+	$(window).on("scroll", setSelectedContent);
+	setSelectedContent.call(window);
+});
+
+function setSelectedContent()
+{
+	var top = $(this).scrollTop();
+	
+	$("#contentList li.selected").removeClass("selected");
+	
+	var minTarget = null;
+	$("#documentArea *[id^=doc]").each(function()
+	{
+		var rect = this.getBoundingClientRect();
+		if($(window).scrollTop() + $(window).height() == $(document).height()) 
+		{
+			minTarget = {distance : rect.top, element : $("#documentArea *[id^=doc]:last")};
+		}
+		else if(rect.top <= 0)
+		{
+			if(!minTarget || minTarget.distance < rect.top)
+			{
+				minTarget = {distance : rect.top, element : this};
+			}
+		}
+	});
+	
+	console.log("민타겟 : ", minTarget);
+	if(minTarget)
+	{
+		var a = $("#contentList li a[href='#" + $(minTarget.element).attr("id") + "']");
+		a.parent().addClass("selected");
+	}
+	else
+	{
+		$("#contentList li:first").addClass("selected");
+	}
+}
