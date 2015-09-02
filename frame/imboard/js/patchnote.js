95cfdbb5e64f25ce0c3bf720992f7715d5e8baa4
Alprensia <tonite32@gmail.com>
2015-09-02 22:31:34 +0900

    임보드 홈페이지 수정

--- frame/imboard/js/patchnote.js
+++ frame/imboard/js/patchnote.js
@@ -1,40 +1,10 @@
 $(document).ready(function()
 {
-	$("#newPatchnote").on("click", function()
-	{
-		var html = $("#newArticleTemplate").html();
-		var template = Handlebars.compile(html);
-		$("#articleBody").prepend(template());
-		
-		TypeWriter.compile({selector:"#content"});
-		
-		$("#saveForm").compile(function(data)
-		{
-			var content = TypeWriter.instances.content.getData().content;
-			data.content = content;
-			data.boardId = "patchnote";
-			data.status = 1;
-			
-			var result = $.api.article.writeArticle(data);
-			if(result.code == 1000)
-			{
-				location.reload();
-			}
-			else
-			{
-				console.error("작성 실패", result);
-			}
-			
-			$(this).show();
-		});
-		
-		$(this).hide();
-	});
-	
 	$("#articleBody form[data-component='form']").compile(function(data)
 	{
 		if(confirm("정말삭제하시겠습니까?"))
 		{
+			data.isRemove = "Y";
 			var result = $.api.article.deleteArticle(data);
 			if(result.code == 1000)
 			{
--- frame/imboard/lib/ckeditor/config.js
+++ frame/imboard/lib/ckeditor/config.js
@@ -25,6 +25,8 @@ CKEDITOR.editorConfig = function( config ) {
 		{ name: 'about' }
 	];
 
+	config.enterMode = CKEDITOR.ENTER_P;
+
 	// The default plugins included in the basic setup define some buttons that
 	// are not needed in a basic editor. They are removed here.
 	config.removeButtons = 'Cut,Copy,Paste,Undo,Redo,Anchor,Underline,Strike,Subscript,Superscript';
