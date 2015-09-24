525d829a8f4a162bc34cabc4b20be4b0c3596127
Alprensia <tonite32@gmail.com>
2015-09-24 18:59:42 +0900

    beforeyougo 수정

--- frame/beforeyougo/js/index.js
+++ frame/beforeyougo/js/index.js
@@ -1,45 +1,9 @@
-var cardPosition = [0, 0, 0];
-
 $(document).ready(function()
 {
-	allocateCard($(".card"), 0);
+	console.log("머야 : ", $.query.fragment);
+	if($.query.fragment)
+	{
+		$(".nav-tabs li").removeAttr("class");
+		$(".nav-tabs li a[href='?fragment=" + $.query.fragment + "']").parent().attr("class", "active");
+	}
 });
-
-function allocateCard(cardList, i)
-{
-	if(i >= cardList.length)
-	{
-		return;
-	}
-	
-	var index = i % cardPosition.length;
-	
-	var card = cardList[i];
-	var prev = $(card).prev();
-	
-	var left = 10;
-	if(index > 0 && i > 0)
-		left += new Number($(prev).css("left").replace("px", "")) + new Number($(prev).css("width").replace("px", ""));
-	
-	var top = cardPosition[index] + 30;
-	
-	$(card).css("position", "absolute").css("left", left + "px").css("top", top + "px").show();
-	
-	var img = $(card).find("img");
-	if(img.length > 0 && img.css("height") == "0px")
-	{
-		img.on("load", function()
-		{
-			var height = $(card).css("height");
-			cardPosition[index] = top + new Number(height.replace("px", ""));
-			
-			allocateCard(cardList, i + 1);
-		});
-	}
-	else
-	{
-		var height = $(card).css("height");
-		cardPosition[index] = top + new Number(height.replace("px", ""));
-		allocateCard(cardList, i + 1);
-	}
-}
