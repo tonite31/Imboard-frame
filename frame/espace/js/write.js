ad2e8bd1739d9353fe4c43d1388c58f9d4053cda
Alprensia <tonite32@gmail.com>
2015-11-09 14:06:31 +0900

    에스파스 수정

--- frame/espace/js/write.js
+++ frame/espace/js/write.js
@@ -1,4 +1,7 @@
 var savedSeq = null;
+
+var selectedResource = null;
+
 $(document).ready(function()
 {
 	savedSeq = $.query.seq;
@@ -110,7 +113,65 @@ $(document).ready(function()
 						
 						var img = document.createElement("img");
 						img.src = src.replace(".gif", ".png");
-						$("#editor").append(img.outerHTML);
+						
+						$(img).on("click load", function()
+						{
+							selectResource(this);
+						});
+						
+						var node = getSelectedNode($("#editor").get(0));
+						if(node.div)
+							$(img).insertAfter(node.div);
+						else
+							$("#editor").append(img);
+					}
+				}
+			},
+			error : function(result)
+			{
+				alert("파일업로드 에러 : " + result);
+			}
+		};
+
+		var result = $.api.article.uploadFile(param);
+		console.log("결과 : ", result);
+	});
+	
+	$("#uploadFile").compile(function(e)
+	{
+		var attachImageData = new FormData();
+		
+		var files = e.target.files;
+		
+		var length = files.length;
+		for(var i=0; i<length; i++)
+		{
+			attachImageData.append("file-" + new Date().getTime() + "-" + i, files[i]);
+		}
+
+		var param = {
+			data : 	attachImageData,
+			success : function(result)
+			{
+				if(result.code != 1000)
+				{
+					alert("파일업로드 에러 발생");
+				}
+				else
+				{
+					result = result.data;
+					for(var i=0; i<result.length; i++)
+					{
+						var src = result[i];
+						
+						var a = document.createElement("a");
+						a.href = src;
+						a.innerText = src.substring(src.lastIndexOf("/"));
+						
+						if(node.div)
+							$(a).insertAfter(node.div);
+						else
+							$("#editor").append(a);
 					}
 				}
 			},
@@ -130,31 +191,131 @@ $(document).ready(function()
 		if(text)
 		{
 			var v = text.replace(/http[s]?:\/\/youtu.be\//gi, "").replace(/http[s]?:\/\/www.youtube.com\/watch\?v=/gi, "");
-			var html = "<iframe data-focus='true' style='width:640px; height:360px;' data-thumbnail-url='http://img.youtube.com/vi/" + v + "/mqdefault.jpg' src='//www.youtube.com/embed/" + v + "' frameborder='0' allowfullscreen></iframe>";
-			$("#editor").append(html);
+			
+			var iframe = document.createElement("iframe");
+			iframe.outerHTML = "<iframe data-focus='true' style='width:640px; height:360px;' data-thumbnail-url='http://img.youtube.com/vi/" + v + "/mqdefault.jpg' src='//www.youtube.com/embed/" + v + "' frameborder='0' allowfullscreen></iframe>";
+			
+			$(iframe).on("click load", function(e)
+			{
+				selectResource(this);
+				e.preventDefault();
+				e.stopPropagation();
+				e.stopImmediatePropagation();
+			});
+			
+			$(iframe).on("blur", function()
+			{
+				selectResource(null);
+			});
+			
+			if(node.div)
+				$(iframe).insertAfter(node.div);
+			else
+				$("#editor").append(iframe);
 //			CKEDITOR.instances.ckeditor.insertHtml(html);
 		}
 	});
 	
+	$("#uploadLink").on("click", function()
+	{
+		var text = prompt("URL을 입력해주세요");
+		if(text)
+		{
+			var a = document.createElement("a");
+			a.href = text;
+			a.innerText = text;
+			
+			if(node.div)
+				$(a).insertAfter(node.div);
+			else
+				$("#editor").append(a);
+		}
+	});
+	
+	$('[data-toggle="tooltip"]').tooltip();
+	
 	$("#editor").on("focus", function()
 	{
-		$("#editor").css("overflow", "none").css("height", "auto").css("min-height", "500px");
+		$("#editor").css("overflow", "none").css("height", "auto").css("min-height", "700px");
 
 		setTimeout(function()
 		{
 			var node = document.getSelection().anchorNode;
-			if(node.nodeName == "DIV")
+			if(node && node.nodeName == "DIV")
 			{
 				$("body").animate({scrollTop : node.getBoundingClientRect().top});
 			}
 		}, 500);
 	});
 	
-	$("#editor").on("blur", function()
+	$("#editor").on("blur", function(e)
 	{
-		$("#editor").css("overflow", "").css("height", "").css("min-height", "");
+		if(e.relatedTarget && $(e.relatedTarget).parent().attr("id") != "toolbar")
+			$("#editor").css("overflow", "").css("height", "").css("min-height", "");
 	});
+	
+	var toolbarOffset = $("#toolbar").offset();
+	var toolbarRect = $("#toolbar").getRect();
+	
+	$(window).on("scroll", function()
+	{
+		if(document.body.scrollTop > toolbarOffset.top)
+		{
+			$("#toolbar").css("position", "fixed").css("top", "0").css("left", toolbarOffset.left + "px").css("width", toolbarRect.width + "px");
+		}
+		else
+		{
+			$("#toolbar").css("position", "").css("top", "").css("left", "");
+		}
+	});
+	
+	setToolbar();
 });
+
+function setToolbar()
+{
+	$("#imageWidth").on("blur keyup", function()
+	{
+		var value = this.value;
+		
+		if(selectedResource)
+		{
+			selectedResource.style.width = value + "px";
+			var style = getComputedStyle(selectedResource);
+			$("#imageHeight").val(style.height.replace("px", ""));
+		}
+	});
+	
+	$("#imageHeight").on("keyup", function()
+	{
+		var value = this.value;
+		
+		if(selectedResource)
+		{
+			selectedResource.style.height = value + "px";
+		}
+	});
+	
+	$("button[data-tool-id]").each(function()
+	{
+		var id = $(this).attr("data-tool-id");
+		
+		$(this).on("click", function()
+		{
+			var selection = getSelectedNode($("#editor").get(0));
+			console.log("셀렉숀 : ", selection);
+			if(id == "bold")
+			{
+				var value = selection.node.nodeValue;
+				
+				var selectedText = value.substring(selection.startOffset, selection.endOffset);
+
+				var html = value.substring(0, selection.startOffset) + "<span style='font-weight:bold;'>" + selectedText + "</span>" + value.substring(selection.endOffset);
+				selection.div.innerHTML = html;
+			}
+		});
+	});
+}
 
 function addTag()
 {
@@ -165,3 +326,85 @@ function addTag()
 	})
 	$("#tag").val("");
 }
+
+function selectResource(el)
+{
+	selectedResource = el;
+	
+	if(el)
+	{
+		var style = getComputedStyle(el);
+		
+		$("#imageWidth").val(style.width.replace("px", ""));
+		$("#imageHeight").val(style.height.replace("px", ""));
+	}
+	else
+	{
+		$("#imageWidth").val("");
+		$("#imageHeight").val("");
+	}
+}
+
+var getSelectedNode = function(editor)
+{
+	if(window.getSelection)
+	{
+		var selection = window.getSelection();
+		if(selection.rangeCount > 0)
+		{
+			var range = selection.getRangeAt(0);
+			var ancestor = range.commonAncestorContainer;
+			if(!contains(editor, ancestor))
+			{
+				var div = document.createElement("div");
+				if(editor.firstChild)
+					editor.insertBefore(div, editor.firstChild);
+				else
+					editor.appendChild(div);
+				
+				return {div : div};
+			}
+			else
+			{
+				var ec = range.endContainer;
+				
+				var startOffset = range.startOffset;
+				var endOffset = range.endOffset;
+				
+				var parent = ec;
+				while((parent = parent.parentElement) && parent != editor && parent.nodeName != "DIV");
+				
+				if(parent == editor)
+				{
+					var div = document.createElement("div");
+					editor.insertBefore(div, ec);
+					div.appendChild(ec);
+					
+					return {div : div, node : ec, startOffset : startOffset, endOffset : endOffset};
+				}
+				else
+				{
+					return {div : parent, node : ec, startOffset : startOffset, endOffset : endOffset};
+				}	
+			}
+		}
+	}
+};
+
+var contains = function(a, b)
+{
+	var target = b;
+	while((target = target.parentElement) && a != target);
+
+	return target == a;
+};
+
+var setCaretPosition = function(node, index)
+{
+	var range = document.createRange();
+	var sel = window.getSelection();
+	range.setStart(node, index);
+	range.collapse(true);
+	sel.removeAllRanges();
+	sel.addRange(range);
+};
